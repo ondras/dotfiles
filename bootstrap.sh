1b81cc8b4d79e3ba2dc59c24c653a0fab7f699f9
Ondrej Zara <ondrej.zara@gmail.com>
2017-10-01 15:07:49 +0200

    bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+DIR=`dirname $0`
+
+FILES=".gitconfig .hgrc .rtorrent.rc .tmux.conf .vimrc .atom"
+for i in $FILES; do
+	ln -s $DIR/$i ~/$i 
+done
+
+BUNDLE=~/.vim/bundle
+mkdir -p $BUNDLE
+cd $BUNDLE
+git clone https://github.com/VundleVim/Vundle.vim.git
