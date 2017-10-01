b807572470ef1e8ab07fe030e3a2e1e60136cf9f
Ondrej Zara <ondrej.zara@gmail.com>
2017-10-01 15:16:04 +0200

    bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 FILE=`readlink -f $0`
 DIR=`dirname $FILE`
 
-FILES=".gitconfig .hgrc .rtorrent.rc .tmux.conf .vimrc .atom"
+FILES=".gitconfig .hgrc .rtorrent.rc .tmux.conf .vimrc .atom .bash_aliases"
 for i in $FILES; do
 	ln -s $DIR/$i ~/$i 
 done
