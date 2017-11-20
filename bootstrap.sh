ea5afc504509456f6c5886609a3cdf301814c539
Ondrej Zara <ondrej.zara@gmail.com>
2017-11-20 17:04:33 +0100

    reorg

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 FILE=`readlink -f $0`
 DIR=`dirname $FILE`
 
-FILES=".gitconfig .hgrc .rtorrent.rc .tmux.conf .vimrc .atom .bash_aliases"
+FILES=".gitconfig .hgrc .rtorrent.rc .tmux.conf .vimrc .bash_aliases"
 for i in $FILES; do
 	ln -s $DIR/$i ~/$i 
 done
@@ -14,11 +14,19 @@ cd $BUNDLE
 git clone https://github.com/VundleVim/Vundle.vim.git
 vim +PluginInstall +qall
 
-# echo "source $DIR/bash_prompt.sh" >> ~/.bashrc
+echo "source $DIR/bash_prompt.sh" >> ~/.bashrc
 
-SUBL="sublime-text-3"
-SUBL_DIR=~/.config/$SUBL/Packages/User
+SUBL=".config/sublime-text-3"
+SUBL_DIR=~/$SUBL/Packages/User
 mkdir -p $SUBL_DIR
 for i in $DIR/$SUBL/* ; do
 	ln -s "$i" $SUBL_DIR
 done
+
+ATOM=".atom"
+ATOM_DIR=~/$ATOM
+mkdir -p $ATOM_DIR
+for i in $DIR/$ATOM/* ; do
+	ln -s "$i" $ATOM_DIR
+done
+
